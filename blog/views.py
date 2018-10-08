f125a07c9d9236ba66c96cc871698cd10db71f27
Treysii <treysilupe8@gmail.com>
2018-10-07 20:05:55 -0600

    Agrego la vista de detalle

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
 
 def lista(request):
     pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar.html', {'pub': pub})
+
+def detalle(request, pk):
+    det = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle.html', {'det': det})
