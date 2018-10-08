53d0b9901b92198d81d4d93c818a812344d6175c
Treysii <treysilupe8@gmail.com>
2018-10-07 19:06:47 -0600

    se agrego la carpeta templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicacion
 
 def lista(request):
-    return render(request, 'blog/listar.html', {})
+    pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar.html', {'pub': pub})
