a508360bf99dc948930370b432e92edf91aa0fb0
Treysii <treysilupe8@gmail.com>
2018-10-07 20:31:15 -0600

    Agrego el formulario/editar/Agregar nueva publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.lista),
     url(r'^detalle/(?P<pk>[0-9]+)/$', views.detalle, name='detalle'),
+    url(r'^post/nuevo/$', views.nuevo, name='nuevo'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.editar, name='editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
+from .forms import PostForm
+from django.shortcuts import redirect
+
 
 def lista(request):
     pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -9,3 +12,29 @@ def lista(request):
 def detalle(request, pk):
     det = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/detalle.html', {'det': det})
+
+def nuevo(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                p = form.save(commit=False)
+                p.autor = request.user
+                p.fecha_publicacion = timezone.now()
+                p.save()
+                return redirect('detalle', pk=p.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/editar.html', {'form': form})
+
+def editar(request, pk):
+        post = get_object_or_404(Publicacion, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('detalle', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/editar.html', {'form': form})
